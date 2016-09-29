64543734c0312d0e49415dd868748e855bfb48a8
Jacob Wasilkowski <jwasilkowski@esri.com>
2016-09-29 08:16:18 -0500

    added c3 chart with 2-way interaction; upgraded to JSAPI 4.1; added custom esri red theme and separated css

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 require([
-  'esri/Color',
-  'esri/graphicsUtils',
   'esri/layers/CSVLayer',
-  'esri/map',
-  'esri/symbols/SimpleMarkerSymbol',
-  'esri/symbols/SimpleLineSymbol',
+  'esri/Map',
   'esri/renderers/SimpleRenderer',
+  'esri/symbols/SimpleMarkerSymbol',
+  'esri/views/MapView',
+
+  'd3/d3.v3.min',
+  'c3/c3.min',
+
   'dojo/domReady!'
 ], function(
-  Color, graphicsUtils, CSVLayer, Map, SimpleMarkerSymbol, SimpleLineSymbol, SimpleRenderer
+  CSVLayer, Map, SimpleRenderer, SimpleMarkerSymbol, MapView,
+  d3, c3
 ) {
-
   var yearSlider = document.getElementById('yearSlider');
   yearSlider.addEventListener('input', function(e) {
     setYear(e.target.value);
@@ -23,127 +25,245 @@ require([
   var yearInfo = document.getElementById('yearInfo');
   var totalInfo = document.getElementById('totalInfo');
 
-  var map = new Map('map', {
-    basemap: 'hybrid',
+  var map = new Map({
+    basemap: 'hybrid'
+  });
+
+  var mapView = new MapView({
+    container: 'viewDiv',
+    map: map,
     center: [-6.93, 61.91],
-    zoom: 9,
-    showInfoWindowOnClick: false
+    zoom: 9
   });
 
   // create the layer for the location center points
-  var csvLocationsLayer = new CSVLayer('resources/FaroeWhaling.csv');
-  var whiteFill = new Color([255, 255, 255]);
-  var blackOutline = new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, new Color([48, 48, 48]), 1);
-  var marker = new SimpleMarkerSymbol('solid', 10, blackOutline, whiteFill);
-  var renderer = new SimpleRenderer(marker);
-  csvLocationsLayer.setRenderer(renderer);
-  map.addLayer(csvLocationsLayer);
+  // var csvLocationsLayer = new CSVLayer({
+  //   url: 'resources/FaroeWhaling.csv',
+  //   renderer: new SimpleRenderer({
+  //     symbol: new SimpleMarkerSymbol({
+  //       size: '10px',
+  //       color: [255, 255, 255],
+  //       outline: {
+  //         color: [48, 48, 48],
+  //         width: 1
+  //       }
+  //     })
+  //   })
+  // });
+  // map.add(csvLocationsLayer);
 
   // create the thematic layer of whale counts
-  var csvThematicLayer = new CSVLayer('resources/FaroeWhaling.csv', {
+  var quantize = d3.scale.quantize().domain([0, 100]).range(d3.range(121));
+  var csvThematicLayer = new CSVLayer({
     copyright: 'www.hagstova.fo',
-    id: 'csvLayer',
+    url: 'resources/FaroeWhaling.csv',
+    id: 'csvThematicLayer',
     fields: [{
-      name: 'year',
-      type: 'Number'
+      name: 'whaling_bay',
+      type: 'string'
     }, {
-      name: 'whales',
-      type: 'Number'
+      name: 'year',
+      type: 'integer'
     }, {
       name: 'hunts',
-      type: 'Number'
+      type: 'double'
+    }, {
+      name: 'whales',
+      type: 'double'
     }, {
       name: 'skinn_values',
-      type: 'Number'
-    }]
+      type: 'double'
+    }],
+    renderer: new SimpleRenderer({
+      symbol: new SimpleMarkerSymbol({
+        // size: 0,
+        color: [178, 0, 255, 0.4],
+        outline: {
+          color: [255, 124, 0],
+          width: 1
+        }
+      }),
+      visualVariables: [{
+        type: 'size',
+        field: 'whales',
+        stops: [{
+          value: 0,
+          size: quantize(9)
+        }, {
+          value: 1,
+          size: quantize(10) + 10
+        }, {
+          value: 25,
+          size: quantize(25)
+        }, {
+          value: 50,
+          size: quantize(50)
+        }, {
+          value: 75,
+          size: quantize(75)
+        }, {
+          value: 100,
+          size: quantize(100)
+        }]
+      }]
+    })
   });
 
-  var purpleFill = new Color([178, 0, 255, 0.4]);
-  var orangeOutline = new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, new Color([255, 124, 0]), 1);
-  var thematicMarker = new SimpleMarkerSymbol('solid', 15, orangeOutline, purpleFill);
-  var thematicRenderer = new SimpleRenderer(thematicMarker);
-  var quantize = d3.scale.quantize().domain([0, 100]).range(d3.range(121));
-  thematicRenderer.setSizeInfo({
-    field: 'whales',
-    // minSize: 0,
-    // maxSize: 100,
-    // minDataValue: 0,
-    // maxDataValue: 200,
-    stops: [{
-      value: 0,
-      size: quantize(0)
-    }, {
-      value: 1,
-      size: quantize(10) + 10
-    }, {
-      value: 25,
-      size: quantize(25)
-    }, {
-      value: 50,
-      size: quantize(50)
-    }, {
-      value: 75,
-      size: quantize(75)
-    }, {
-      value: 100,
-      size: quantize(100)
-    }]
-  });
-  csvThematicLayer.setRenderer(thematicRenderer);
+  map.add(csvThematicLayer);
 
-  map.addLayer(csvThematicLayer);
+  var csvLayerView,
+    csvLayerViewGraphics,
+    chartData = {},
+    chart;
+  mapView.on('layerview-create', function(e) {
+    if (e.layer.id === 'csvThematicLayer') {
+      csvLayerView = e.layerView;
 
-  csvLocationsLayer.on('click,mouse-move', function(e) {
-    var attributes = e.graphic.attributes;
-    bayInfo.innerHTML = attributes.whaling_bay;
-  });
-  csvLocationsLayer.on('mouse-out', function() {
-    cleanupInfo();
-  });
-
-  csvThematicLayer.on('click,mouse-move', function(e) {
-    var attributes = e.graphic.attributes;
-    bayInfo.innerHTML = attributes.whaling_bay + ', ' + attributes.year;
-    whalesInfo.innerHTML = attributes.whales + ' whales';
-    huntsInfo.innerHTML = attributes.hunts + ' hunts ';
-    skinnInfo.innerHTML = attributes.skinn_values + ' skinn';
-  });
-  csvThematicLayer.on('mouse-out', function() {
-    cleanupInfo();
-  });
-
-  map.on('layer-add', function(e) {
-    if (e.layer.id === 'csvLayer') {
-      setYear(1996);
       bayInfo.innerHTML = instructionalText;
       document.getElementById('mapControls').style.display = 'block';
-      e.target.setExtent(graphicsUtils.graphicsExtent(e.layer.graphics), true);
+
+      // get access to all the graphics
+      csvLayerView.queryGraphics().then(function(graphics) {
+        csvLayerViewGraphics = graphics;
+
+        csvLayerViewGraphics.forEach(function(graphic) {
+          if (!chartData.hasOwnProperty(graphic.attributes.year)) {
+            chartData[graphic.attributes.year] = {};
+            chartData[graphic.attributes.year].whaleTotal = 0;
+            chartData[graphic.attributes.year].huntTotal = 0;
+          };
+          chartData[graphic.attributes.year].whaleTotal += graphic.attributes.whales;
+          chartData[graphic.attributes.year].huntTotal += graphic.attributes.hunts;
+        });
+
+        chart = c3.generate({
+          bindTo: '#chart',
+          data: {
+            x: 'x',
+            columns: [
+              ['x'].concat(Object.keys(chartData)),
+              ['whales'].concat(Object.keys(chartData).map(function(year) {
+                return (chartData[year].whaleTotal)
+              })),
+              // ['hunts'].concat(Object.keys(chartData).map(function(year) {
+              //   return (chartData[year].huntTotal)
+              // }))
+            ],
+            axes: {
+              whales: 'y',
+              hunts: 'y2'
+            },
+            type: 'spline',
+            colors: {
+              whales: '#000000',
+              hunts: '#ffffff'
+            },
+            selection: {
+              enabled: true
+            },
+            onclick: function(d) {
+              yearSlider.value = d.x;
+              setYear(d.x);
+            }
+          },
+          axis: {
+            x: {
+              show: false
+            },
+            y: {
+              show: false
+            },
+            y2: {
+              show: false
+            }
+          },
+          point: {
+            show: false,
+            select: {
+              r: 3.5
+            }
+          },
+          legend: {
+            show: false
+          },
+          tooltip: {
+            show: false
+          },
+          size: {
+            height: 75,
+            width: 220
+          }
+        });
+
+        setYear(1996);
+
+      });
     }
   });
 
-  function cleanupInfo() {
-    bayInfo.innerHTML = instructionalText;
-    whalesInfo.innerHTML = '';
-    huntsInfo.innerHTML = '';
-    skinnInfo.innerHTML = '';
-  };
+  mapView.on("click", function(evt) {
+    var screenPoint = evt.screenPoint;
+    mapView.hitTest(screenPoint).then(function(response) {
+      var graphic = response.results[0].graphic;
 
+      bayInfo.innerHTML = graphic.attributes.whaling_bay + ', ' + graphic.attributes.year;
+      whalesInfo.innerHTML = graphic.attributes.whales + ' whales';
+      huntsInfo.innerHTML = graphic.attributes.hunts + ' hunts ';
+      skinnInfo.innerHTML = graphic.attributes.skinn_values + ' skinn';
+    });
+  });
 
   function setYear(year) {
     yearInfo.innerHTML = year;
 
     year = Number(year);
+
+    // toggle graphic visibility and add up the year's total whales
     var sumWhales = 0;
-    csvThematicLayer.graphics.forEach(function(g) {
+    csvLayerViewGraphics.forEach(function(g) {
       if (g.attributes.year === year) {
-        g.show();
+        g.visible = true;
         sumWhales += g.attributes.whales;
       } else {
-        g.hide();
+        g.visible = false;
       }
     });
 
     totalInfo.innerHTML = sumWhales + ' whales';
+
+    // workaround to "refresh" the csv layer view after changing graphic properties
+    csvLayerView.visible = false;
+    setTimeout(function() {
+      csvLayerView.visible = true;
+    }, 5);
+
+    chart.select(['whales'], [Object.keys(chartData).indexOf(String(year))], true);
   };
+
+  // csvLocationsLayer.on('click,mouse-move', function(e) {
+  //   var attributes = e.graphic.attributes;
+  //   bayInfo.innerHTML = attributes.whaling_bay;
+  // });
+  // csvLocationsLayer.on('mouse-out', function() {
+  //   cleanupInfo();
+  // });
+  //
+  // csvThematicLayer.on('click,mouse-move', function(e) {
+  //   var attributes = e.graphic.attributes;
+  //   bayInfo.innerHTML = attributes.whaling_bay + ', ' + attributes.year;
+  //   whalesInfo.innerHTML = attributes.whales + ' whales';
+  //   huntsInfo.innerHTML = attributes.hunts + ' hunts ';
+  //   skinnInfo.innerHTML = attributes.skinn_values + ' skinn';
+  // });
+  // csvThematicLayer.on('mouse-out', function() {
+  //   cleanupInfo();
+  // });
+  //
+  // function cleanupInfo() {
+  //   bayInfo.innerHTML = instructionalText;
+  //   whalesInfo.innerHTML = '';
+  //   huntsInfo.innerHTML = '';
+  //   skinnInfo.innerHTML = '';
+  // };
 
 });
